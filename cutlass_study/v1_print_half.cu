5fdda8605c7f5dfe822890f36a90eafe317c833e
shiyi0113 <shiyileshengxai@gmail.com>
2025-05-14 15:56:46 +0800

    test cutlass

---
+++ cutlass_study/v1_print_half.cu
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cutlass/cutlass.h>
+#include <cutlass/numeric_types.h>
+#include <cutlass/core_io.h>
+
+int main()
+{
+    cutlass::half_t x = 2.25_hf;
+    std::cout << x << std::endl;
+}
